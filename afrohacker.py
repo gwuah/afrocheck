fdf91e018bf477549794f8ade3308838ba884775
Griffith Awuah <griffith@dolphtech.com>
2017-11-01 04:08:08 +0000

    removed api keys/tokens

--- afrohacker.py
+++ afrohacker.py
@@ -2,10 +2,10 @@ import tweepy
 
 # api keys/tokens here pls
 # I am not father christmas
-API_KEY = '8Qvtc4uE2aVeRnQ0c1WMMBm2o'
-API_TOKEN = 'JVMdC3WNs6ytxIT7itJU4icOZuCC4rPjaZSseqZigALPD0Pbzr'
-ACCESS_TOKEN = '822769558931079169-eycjRZ8Yb9LTV7MSUASVWmpuqmzX5iV'
-ACCESS_TOKEN_SECRET = 'FmEJf5U9OlWv7bkcNPwszAPo8euB3ZUQTZHcn0dn56Loi'
+API_KEY = ''
+API_TOKEN = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
 
 # twitter handle of the colony leaders!
 KINGS_NAME = "gwuah_"
